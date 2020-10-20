fb36f33369429005a1badd7e9830c7dfadaff704
abbasi-naim <abbasi.naim@gmail.com>
2020-10-20 21:46:11 +0330

    Create Database Connection

---
+++ public/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "naim_abbasi";
+$password = "P@ssw0rd";
+$dbname = "BookStore";
+
+//Create Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
