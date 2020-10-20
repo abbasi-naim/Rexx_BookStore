9780ad66e99daf1fcdc2efa416a54f1035592e6e
abbasi-naim <abbasi.naim@gmail.com>
2020-10-20 22:29:33 +0330

    changing the code to show the single quote data by adding real scape

--- importjson.php
+++ importjson.php
@@ -12,6 +12,7 @@ require_once("db.php");
 
 foreach ($json_a as $person_name => $person_a) {
 
+    //changing the code to show the single quote data by adding real scape 
     $sql="INSERT INTO `sale` (`sale_id`, `customer_name`, `customer_mail`, `product_id`, `product_name`, `product_price`, `sale_date`, `version`) VALUES (".$person_a["sale_id"].", '".$person_a["customer_name"]."', '".$person_a["customer_mail"]."', '".$person_a["product_id"]."', '".mysqli_real_escape_string($conn,$person_a["product_name"])."', '".$person_a["product_price"]."', '".$person_a["sale_date"]."', '".$person_a["version"]."');";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
