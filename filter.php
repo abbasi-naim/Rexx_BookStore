453f8461887f6a5258278b7198511a233e2d5454
abbasi-naim <abbasi.naim@gmail.com>
2020-10-20 22:24:29 +0330

    Adding footer for rexx-system

--- filter.php
+++ filter.php
@@ -93,5 +93,14 @@ $conn->close();
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+<footer>
+<div class="d-flex justify-content-center align-items-center container ">
+<div class="footer-row">
+<div class="footer-5 footer-copyright" style="color: #c61c1d;">© rexx systems GmbH, 2020</div>
+</div>
+</div>
+</footer>
+
 </body>
 </html>
